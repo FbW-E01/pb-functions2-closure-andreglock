b11937e58f85f3d3b62f90a624be1028547c64f1
André <andreglock@gmail.com>
2021-05-12 16:03:40 +0200

    added number 3 in python

--- index.py
+++ index.py
@@ -2,5 +2,5 @@ def createBase(input):
     return lambda number: print(number + input)
 
 addSix = createBase(6)
-addSix(10); # returns 16
-addSix(21); # returns 27
+addSix(10) # returns 16
+addSix(21) # returns 27
